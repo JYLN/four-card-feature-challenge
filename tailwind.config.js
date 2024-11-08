89cd41866e297b3726103e89c1d60c4ed1a259b4
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2024-11-08 12:40:45 -0700

    style: :lipstick: update global styles and TailwindCSS config with custom font and colors
    - Import Poppins font from fontsource package in styles.css
    - Update Tailwind config with color pallette from design guide
    - Extend Tailwind config with Poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    colors: {
+      red: "#EA5454",
+      green: "#44D3D2",
+      yellow: "#FCAE4A",
+      blue: "#549EF2",
+      white: "#FFFFFF",
+      lotion: "#FAFAFA",
+      primary: "#4D4F62",
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
